50068e56e0733a2c5a058d301c052552bc380f27
Civilis317 <ljwzeilstra@gmail.com>
2023-02-26 20:15:52 +0100

    added docker login

--- vars/build.groovy
+++ vars/build.groovy
@@ -26,6 +26,8 @@ def call(String... args) {
 
       stage('Docker Image') {
         steps {
+          withCredentials([usernamePassword(credentialsId: 'DOCKER_REGISTRY_LAVONDYSS_CREDENTIALS', passwordVariable: 'password', usernameVariable: 'username')]) { 
+          sh "docker login -u $username -p $password"
           sh '''
              cd docker/image
              ls
